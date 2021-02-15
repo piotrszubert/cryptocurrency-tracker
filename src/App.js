9317e0773adab2cb8f59a1c4f0a49c8f6107c146
Piotr Szubert <piotr.szubert777@gmail.com>
2021-02-15 17:25:03 +0100

    display data from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 /*
@@ -8,19 +8,38 @@ vs_currency=pln&order=market_cap_desc&per_page
 =100&page=1&sparkline=false
 */
 
-function App() {
+const App = () => {
 
-  axios.get(
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=50&page=1&sparkline=false'
-  ).then(res => {
-    console.log(res.data);
-  }).catch(err => console.log(err));
+  const [items, setItems] = useState([]);
+  //const [isLoading, setIsLoading] = useState(true);
 
-return (
-  <div>
-
-  </div>
-);
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=50&page=1&sparkline=false`)
+      setItems(result.data);
+      // setIsLoading(false);
+    }
+    fetchItems();
+  }, [])
+  /* 
+    axios.get(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+    ).then(res => {
+      console.log(res.data);
+    }).catch(err => console.log(err));
+   */
+  return (
+    <div items={items}>
+      {items.map((item) => (
+        <div className="item" key={item.id}>
+            <h2>{item.name}</h2>
+            <h3>{item.symbol}</h3>
+            <p>{item.current_price}</p>
+            <p>{item.price_change_24h}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
